f7af888aab3afe4d9b34d9b995e14cd6dac27b85
recolte00 <yukiyako2.lulu1920@gmail.com>
2021-02-14 03:12:18 +0900

    [add] EditCalendar

--- PoiTest/src/main/java/POI/MainEdit.java
+++ PoiTest/src/main/java/POI/MainEdit.java
@@ -3,6 +3,7 @@ package POI;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.math.BigDecimal;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -17,7 +18,7 @@ public class MainEdit {
 		String sheetName = "シートA";
 		try {
 			// 使用例１：物理ファイルをtemplateにする場合
-			File template = new File("testfile.xlsx");
+			File template = new File("calendarSample.xlsx");
 			WorkbookWrapper wr = new WorkbookWrapper(template);
 			
 			// 使用例２：メモリ上のWorkbookをtemplateにする場合
@@ -46,16 +47,32 @@ public class MainEdit {
 	 * @return
 	 */
 	private static List<DataDto> prepareData() {
-		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM");
+		String startDate = "2021/02"//+"/01 00:00:00"
+		;
 		Calendar cal = Calendar.getInstance();
+		Date date1 = toDate(startDate,"yyyy/MM");
+		cal.setTime(date1);
+		cal.add(Calendar.MONTH, 1);
+		Date date2 = cal.getTime();
 		List<DataDto> dataDtos = new ArrayList<DataDto>();
 		for (int i=0; i<50000; i++) {
-			dataDtos.add(new DataDto("こんにちは", (double)i/100, new BigDecimal("-0.5"), df.format(new Date()), df.format(cal.getTime()), String.format("%1$02d", i%47+1)));
-			dataDtos.add(new DataDto("", (double)i/100, new BigDecimal("0.5"), null, df.format(cal.getTime()), String.format("%1$02d", i%47+1)));
+			dataDtos.add(new DataDto("こんにちは", (double)i/100, new BigDecimal("-0.5"), String.valueOf(date1), String.valueOf(date2), String.format("%1$02d", i%47+1)));
+			dataDtos.add(new DataDto("", (double)i/100, new BigDecimal("0.5"), null, String.valueOf(date1), String.format("%1$02d", i%47+1)));
 		}
 		return dataDtos;
 	}
-
+	
+	public static Date toDate(String str, String format)throws ParseException{
+		SimpleDateFormat df = new SimpleDateFormat(format);
+		df.setLenient(false);
+		Date date = new Date();
+		try {
+			date = df.parse(str);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return date;
+	}
 	/**
 	 * テスト用ファイル名生成
 	 * @return
